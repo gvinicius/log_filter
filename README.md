a5c1480f51cc4943f912945766268fa8f7e108c5
Vinicius Lins <vlinsufba@gmail.com>
2021-10-01 13:26:35 -0300

    Add parser

---
+++ README.md
@@ -0,0 +1,17 @@
+### Log Filter
+
+Firter logs by using ruby.
+
+## Setup
+```# install ruby 3.0.2
+```
+
+``` bundle install
+```
+
+``` bundle exec rspec # for testing
+```
+
+## Run
+``` ruby ./parser.rb
+```
---
+++ parser.rb
@@ -0,0 +1,13 @@
+require_relative 'operator'
+
+DEFAULT_SEPARATOR = ' '
+filename = ARGV[0]
+
+puts 'Reading a log file'
+operator = Operator.new(filename, DEFAULT_SEPARATOR)
+
+puts 'Most viewed pages'
+operator.tabulate.map { |element| puts "#{element.keys.first} - #{element.values.first}" }
+
+puts 'Most viewed pages (unique accesses)'
+operator.tabulate_unique.map { |element| puts "#{element.keys.first} - #{element.values.first}" }
